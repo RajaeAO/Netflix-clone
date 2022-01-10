deda200d52b377ce60c603b6ee8bd8695805e8cd
RajaeAO <shikazukako1393@gmail.com>
2022-01-10 21:12:22 +0100

    finalization of my netflix clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import "./App.scss";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
 import Footer from "./components/Footer";
+import Video from "./components/Video";
+import requests from "./config/Requests";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-      <div className="test"></div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Nav />
+            <Banner />
+            <Row
+              title="Programmes originaux Netflix"
+              fetchUrl={requests.fetchNetflixOriginals}
+              isPoster={true}
+            />
+            <Row
+              title="Tendances actuelles"
+              fetchUrl={requests.fetchTrending}
+            />
+            <Row title="Les mieux notés" fetchUrl={requests.fetchTopRated} />
+            <Row title="Films d'action" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Films d'horreur" fetchUrl={requests.fetchTrending} />
+            <Row title="Comédies" fetchUrl={requests.fetchTopRated} />
+            <Row title="Documentaires" fetchUrl={requests.fetchActionMovies} />
 
-      {/*banner*/}
-
-      {/*rows*/}
-
-      {/*video*/}
-
-      {/* quick view */}
-
-      <Footer />
+            <Footer />
+          </Route>
+          <Route path="/video/:id" component={Video} />
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Banner.scss";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import HelpIcon from "@mui/icons-material/Help";
 import requests from "../config/Requests";
 import axios from "axios";
-import { AlignHorizontalRightRounded } from "@mui/icons-material";
+import QuickView from "./QuickView";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
+  const [popup, setPopup] = useState(false);
+
+  function handlePopup() {
+    popup ? setPopup(false) : setPopup(true);
+  }
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchTrending);
@@ -22,31 +29,44 @@ function Banner() {
   }, []);
 
   function truncateText(string, n) {
-    return string?.length > n
-      ? string.substr(0, n - 1) + "..."
-      : "no description";
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
-  console.log(movie);
+  const bannerStyle = {
+    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  };
   return (
-    <header className="banner">
+    <header className="banner" style={bannerStyle}>
       <div className="banner__content">
         <h1 className="banner__title">
-          {movie?.title || movie?.original_title}
+          {movie?.title || movie?.name || movie?.original_title}
         </h1>
         <p className="banner__description">
           {truncateText(movie?.overview, 100)}
         </p>
-        <div className="banner__buttons">
-          <button className="banner__button banner__button--play">
-            <PlayArrowIcon />
-            play
-          </button>
-          <button className="banner__button">
-            <HelpIcon />
-            more info
-          </button>
-        </div>
+        {movie.id && (
+          <div className="banner__buttons">
+            <Link to={`/video/${movie?.id}`}>
+              <button className="banner__button banner__button--play">
+                <PlayArrowIcon />
+                play
+              </button>
+            </Link>
+            <button className="banner__button" onClick={handlePopup}>
+              <HelpIcon />
+              more info
+            </button>
+          </div>
+        )}
       </div>
+
+      <QuickView
+        bannerStyle={bannerStyle}
+        movie={movie}
+        popupStatus={popup}
+        popup={handlePopup}
+      />
     </header>
   );
 }
